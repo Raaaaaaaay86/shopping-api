fd12470aba61c46f749361820cba029a263bfbb7
Ray Lin <mirror46258@gmail.com>
2022-03-27 21:32:42 +0800

    feat: Update DefaultResponse constructor

--- src/main/java/com/example/shoppingapi/dto/DefaultResponse.java
+++ src/main/java/com/example/shoppingapi/dto/DefaultResponse.java
@@ -1,10 +1,14 @@
 package com.example.shoppingapi.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DefaultResponse {
     Boolean status;
     String message;
