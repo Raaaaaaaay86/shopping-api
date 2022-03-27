5f2bb312d2def0796fe067f7e8d26a2019a4692d
Ray Lin <mirror46258@gmail.com>
2022-03-27 21:33:41 +0800

    feat: Update UserService methods. Mainly revise the register and login logic

--- src/main/java/com/example/shoppingapi/service/UserService.java
+++ src/main/java/com/example/shoppingapi/service/UserService.java
@@ -2,7 +2,10 @@ package com.example.shoppingapi.service;
 
 import com.example.shoppingapi.entity.User;
 
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
 public interface UserService {
-    Boolean login(String username, String password);
+    Optional<String> login(String username, String password);
     User register(User newUser) throws Exception;
 }
--- src/main/java/com/example/shoppingapi/service/UserServiceImpl.java
+++ src/main/java/com/example/shoppingapi/service/UserServiceImpl.java
@@ -2,8 +2,22 @@ package com.example.shoppingapi.service;
 
 import com.example.shoppingapi.dao.UserRepository;
 import com.example.shoppingapi.entity.User;
+import com.example.shoppingapi.entity.UserRole;
+import com.example.shoppingapi.utils.jwt.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -11,9 +25,32 @@ public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    JWTUtil jwtUtil;
+
+    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
     @Override
-    public Boolean login(String username, String password) {
-        return null;
+    public Optional<String> login(String username, String password) {
+        var authentication =  authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+        if (!authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        var roles = ((UserDetails) authentication.getPrincipal())
+                .getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.toList());
+        var newJWTToken = jwtUtil.createToken(username, roles);
+
+        return Optional.of(newJWTToken);
     }
 
     @Override
@@ -26,6 +63,9 @@ public class UserServiceImpl implements UserService {
         if (savedUserOptional.isPresent()) {
             throw new Exception("Username already exists");
         }
+        var userRole = new UserRole("ROLE_USER", "user");
+        newUser.setPassword(encoder.encode(newUser.getPassword()));
+        newUser.setRoles(List.of(userRole));
 
         return userRepository.save(newUser);
     }
