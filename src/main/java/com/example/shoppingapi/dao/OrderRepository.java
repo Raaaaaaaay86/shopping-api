eb291bd38a173d45219d352e40b06173e8c05917
Ray Lin <mirror46258@gmail.com>
2022-03-27 13:14:52 +0800

    refactor: remove OrderRepository. - Reason: Already rename to OrderDetailRepository

--- src/main/java/com/example/shoppingapi/dao/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.shoppingapi.dao;
-
-import com.example.shoppingapi.entity.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<OrderDetail, Long> {
-}
